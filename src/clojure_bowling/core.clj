ccbfdeb181763db1b6bf037d48838cb18d1aebbd
jnuank <ikuta1919@gmail.com>
2026-09-14 23:21:43 +0900

    initial

---
+++ src/clojure_bowling/core.clj
@@ -0,0 +1,12 @@
+(ns clojure-bowling.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Ho, Wd!"))
+
+(println "Hello, World!")
+
+
+(-main)
---
+++ test/clojure_bowling/core_test.clj
@@ -0,0 +1,11 @@
+(ns clojure-bowling.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-bowling.core :refer :all]))
+
+; TODO: bowring 
+; 
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
