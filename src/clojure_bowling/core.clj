0424bee3145ee14fa080ce8f00794d0e3c68bfd1
jnuank <ikuta1919@gmail.com>
2026-09-15 05:18:02 +0900

    はじめてのガーター

--- src/clojure_bowling/core.clj
+++ src/clojure_bowling/core.clj
@@ -1,9 +1,13 @@
 (ns clojure-bowling.core
   (:gen-class))
 
+(defn score [throws]
+  0)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Ho, Wd!"))
+
 
 
--- test/clojure_bowling/core_test.clj
+++ test/clojure_bowling/core_test.clj
@@ -1,14 +1,24 @@
 (ns clojure-bowling.core-test
   (:require [clojure.test :refer :all]
-            [clojure-bowling.core :refer :all]))
+            [clojure-bowling.core :refer [score]]))
 
 ; TODO: bowring 
-; gutter
-
+; gutter 0
+; 10回投げて すべて 0  の場合は 0 を返す
+; 特別なものはなし
+; 10回投げて すべて1 の場合は 20 を返す
 
 (deftest score-test
   (testing "score should be 0 for gutter game"
-    (is (= 2 1)))
-  (testing "どうだ"
-    (is (= 2 3))))
+    (is (= (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]) 0))))
 
+
+; スペア
+; 1回投げてスペア。2回目はガーター。その後0なら10を返す
+
+
+
+; ストライク
+; 1回投げてストライク。2回目はガーター。その後0なら10を返す
+; 1回投げてストライク。2回目は9。その後0なら19を返す
+; 1回投げてストライク。2回目はスペア。その後0なら20を返す
