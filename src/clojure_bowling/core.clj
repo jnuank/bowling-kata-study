1d76de288526550c1f851e749b4756e79e6c55c2
jnuank <ikuta1919@gmail.com>
2026-09-15 08:35:55 +0900

    condに入れ替えた

--- src/clojure_bowling/core.clj
+++ src/clojure_bowling/core.clj
@@ -7,8 +7,13 @@
     (let [first-roll (first throws)
           second-roll (second throws)
           rest-game (drop 2 throws)]
-      (if (= 10 (+ first-roll second-roll))
-        (+ 10 (nth throws 2)
-           (score rest-game))
-        (+ first-roll second-roll
-           (score rest-game))))))
+      (cond
+        (= 10 first-roll)
+        (score (drop 1 throws))
+
+        (= 10 (+ first-roll second-roll))
+        (+ 10 (nth throws 2) (score (drop 2 throws)))
+        
+        :else
+        (+ first-roll  second-roll
+           (score (drop 2 throws)))))))
