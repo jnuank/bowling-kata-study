a228e63fbe8870cef3785ad1edefdcb38eee4861
jnuank <ikuta1919@gmail.com>
2026-09-15 22:00:49 +0900

    tidy

--- src/clojure_bowling/core.clj
+++ src/clojure_bowling/core.clj
@@ -10,7 +10,7 @@
           spare? (= 10 (+ first-roll second-roll))]
       (cond
         strike?
-        (+ 10 (nth throws 1) (nth throws 2) (score (drop 1 throws)))
+        (+ 10 (reduce + (take 2 (rest throws))) (score (rest throws)))
 
         spare?
         (+ 10 (nth throws 2) (score (drop 2 throws)))
