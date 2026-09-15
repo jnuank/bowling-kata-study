cde14c580dece1d989f7449069737cf1496caace
jnuank <ikuta1919@gmail.com>
2026-09-15 22:56:16 +0900

    tidy

--- src/clojure_bowling/core.clj
+++ src/clojure_bowling/core.clj
@@ -1,32 +1,31 @@
 (ns clojure-bowling.core
   (:gen-class))
 
-(defn- score* [throws frame]
+(defn- frame-kind [throws]
   (let [first-roll (first throws)
         second-roll (second throws)
         strike? (= 10 first-roll)
         spare? (= 10 (+ first-roll second-roll))]
-    (if (== 10 frame)
-      (cond
-        strike?
-        (+ 10 (reduce + (take 2 (rest throws))))
+    (cond
+      strike? :strike
+      spare? :spare
+      :else :open)))
 
-        spare?
-        (+ 10 (nth throws 2))
+(defn- frame-points [throws]
+  (case (frame-kind throws)
+    :strike (+ 10 (reduce + (take 2 (rest throws))))
+    :spare (+ 10 (nth throws 2))
+    :open (+ (first throws) (second throws))))
 
-        :else
-        (+ first-roll second-roll))
+(defn- remaining-throws [throws]
+  (case (frame-kind throws)
+    :strike (rest throws)
+    (drop 2 throws)))
 
-      (cond
-        strike?
-        (+ 10 (reduce + (take 2 (rest throws))) (score* (rest throws) (inc frame)))
-
-        spare?
-        (+ 10 (nth throws 2) (score* (drop 2 throws) (inc frame)))
-
-        :else
-        (+ first-roll  second-roll
-           (score* (drop 2 throws) (inc frame)))))))
+(defn- score* [throws frame]
+  (if (== 10 frame)
+    (frame-points throws)
+    (+ (frame-points throws) (score* (remaining-throws throws) (inc frame)))))
 
 (defn score [throws]
   (if (empty? throws)
