646663ecd486e31458599fce4bf1cf14a42044b9
jnuank <ikuta1919@gmail.com>
2026-09-15 21:49:42 +0900

    なんとか通す

--- src/clojure_bowling/core.clj
+++ src/clojure_bowling/core.clj
@@ -8,7 +8,7 @@
           second-roll (second throws)]
       (cond
         (= 10 first-roll)
-        (+ 10 (score (drop 1 throws)))
+        (+ 10 (nth throws 1) (nth throws 2) (score (drop 1 throws)))
 
         (= 10 (+ first-roll second-roll))
         (+ 10 (nth throws 2) (score (drop 2 throws)))
