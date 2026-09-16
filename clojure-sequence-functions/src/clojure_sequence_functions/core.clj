95f14eb1ec42b3e63601f6765fca48d76fff74f1
jnuank <ikuta1919@gmail.com>
2026-09-16 21:02:58 +0900

    ten-frameの概念

--- clojure-sequence-functions/src/clojure_sequence_functions/core.clj
+++ clojure-sequence-functions/src/clojure_sequence_functions/core.clj
@@ -3,7 +3,9 @@
 
 (defn score [pins]
   (let [spare? (= 10 (reduce + (take 2 pins)))
-        ]
-  (if spare?
-    (+ 10 (first (drop 2 pins)) (score (drop 2 pins)))
-    (reduce + pins))))
+        ten-frame? (= 3 (count pins))]
+    (if ten-frame?
+      (reduce + pins)
+      (if spare?
+        (+ 10 (first (drop 2 pins)) (score (drop 2 pins)))
+        (reduce + pins)))))
