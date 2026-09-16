3416122aa5385a80cc61ec6803eb37ca1232878e
jnuank <ikuta1919@gmail.com>
2026-09-16 20:46:28 +0900

    nthやめる

--- clojure-sequence-functions/src/clojure_sequence_functions/core.clj
+++ clojure-sequence-functions/src/clojure_sequence_functions/core.clj
@@ -2,7 +2,8 @@
   (:gen-class))
 
 (defn score [pins]
-  (let [spare? (= 10 (reduce + (take 2 pins)))]
+  (let [spare? (= 10 (reduce + (take 2 pins)))
+        ]
   (if spare?
-    (+ 10 (nth pins 2) (score (drop 2 pins)))
+    (+ 10 (first (drop 2 pins)) (score (drop 2 pins)))
     (reduce + pins))))
