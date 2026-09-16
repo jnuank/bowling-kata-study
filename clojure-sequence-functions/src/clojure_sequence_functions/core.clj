2d2c9d336d672917ceeb14fcce0afe92cc4db8ae
jnuank <ikuta1919@gmail.com>
2026-09-17 08:47:01 +0900

    中身を軽ｓた

--- clojure-sequence-functions/src/clojure_sequence_functions/core.clj
+++ clojure-sequence-functions/src/clojure_sequence_functions/core.clj
@@ -3,24 +3,21 @@
 
 
 (defn- score* [pins frame]
-  (cond 
+  (cond
     (empty? pins)
     0
-    
+
     (= 10 frame)
     (reduce + pins)
-    
-    :else 
+
+    :else
     (let [spare? (= 10 (reduce + (take 2 pins)))
           strike? (= 10 (first pins))
-          ten-frame? (= 10 frame)
           next-frame (inc frame)]
-      (if ten-frame?
-        (reduce + pins)
-        (cond
-          strike? (+ 10 (reduce + (take 2 (drop 1 pins))) (score* (drop 1 pins) next-frame))
-          spare? (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) next-frame))
-          :else (+ (reduce + (take 2 pins)) (score* (drop 2 pins) next-frame)))))))
+      (cond
+        strike? (+ 10 (reduce + (take 2 (drop 1 pins))) (score* (drop 1 pins) next-frame))
+        spare? (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) next-frame))
+        :else (+ (reduce + (take 2 pins)) (score* (drop 2 pins) next-frame))))))
 
 (defn score [pins]
   (score* pins 1))
