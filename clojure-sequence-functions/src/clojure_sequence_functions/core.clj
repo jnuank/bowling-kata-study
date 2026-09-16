d13d3ea6d46a8e5117fa248f926ebf86c944dcba
jnuank <ikuta1919@gmail.com>
2026-09-17 08:29:06 +0900

    frame-countの概念

--- clojure-sequence-functions/src/clojure_sequence_functions/core.clj
+++ clojure-sequence-functions/src/clojure_sequence_functions/core.clj
@@ -6,13 +6,13 @@
     0
     (let [spare? (= 10 (reduce + (take 2 pins)))
           strike? (= 10 (first pins))
-          ten-frame? (= 3 (count pins))]
+          ten-frame? (= 10 frame-count)]
       (if ten-frame?
         (reduce + pins)
         (cond
-          strike? (+ 10 (reduce + (take 2 (drop 1 pins))) (score* (drop 1 pins) 1))
-          spare? (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) 1))
-          :else (+ (reduce + (take 2 pins)) (score* (drop 2 pins) 1)))))))
+          strike? (+ 10 (reduce + (take 2 (drop 1 pins))) (score* (drop 1 pins) (inc frame-count)))
+          spare? (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) (inc frame-count)))
+          :else (+ (reduce + (take 2 pins)) (score* (drop 2 pins) (inc frame-count))))))))
 
 (defn score [pins]
-  (score* pins 0))
+  (score* pins 1))
