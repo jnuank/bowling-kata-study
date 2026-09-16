d2f8934883f8f869903302ad538e3d6e78da4f5d
jnuank <ikuta1919@gmail.com>
2026-09-16 20:29:40 +0900

    9pi

--- clojure-sequence-functions/spec/clojure_sequence_functions/core_spec.clj
+++ clojure-sequence-functions/spec/clojure_sequence_functions/core_spec.clj
@@ -1,7 +1,12 @@
 (ns clojure-sequence-functions.core-spec
-  (:require [speclj.core :refer :all]
-            [clojure-sequence-functions.core :refer :all]))
+  (:require [speclj.core :refer [describe it should=]]
+            [clojure-sequence-functions.core :refer [score]]))
 
 (describe "core"
-  (it "should return the first element of a sequence"
-    (should= 1 (first [1 2 3]))))
+          (it "should return the first element of a sequence"
+              (should= 1 (first [1 2 3]))))
+
+
+(describe "score"
+          (it "全フレーム9ピン"
+              (should= 90 (score [9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0 9 0]))))
