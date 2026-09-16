6dbe2dc1a9954bdf6554528e11d3882a40e3573b
jnuank <ikuta1919@gmail.com>
2026-09-16 20:09:35 +0900

    爆誕

---
+++ clojure-sequence-functions/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-sequence-functions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-sequence-functions/test/clojure_sequence_functions/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-sequence-functions.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-sequence-functions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
