8b9c91adf7db8ae20e6bd2e6e7d0610a1967b1b7
jnuank <ikuta1919@gmail.com>
2026-09-17 12:13:24 +0900

    少し買える

--- clojure-destructuring/spec/clojure_destructuring/core_spec.clj
+++ clojure-destructuring/spec/clojure_destructuring/core_spec.clj
@@ -1,7 +1,7 @@
 (ns clojure-destructuring.core-spec
   (:require [clojure-destructuring.core :refer :all]
-            [speclj.core :refer :all]))
+            [speclj.core :refer [describe, it, should=]]))
 
 (describe "test"
           (it "test2"
-              (should= 1 2)))
+              (should= 2 2)))
