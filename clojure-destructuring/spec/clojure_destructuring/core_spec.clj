f1fd49ba96e4acffce63a43eeb5f79c9067f7265
jnuank <ikuta1919@gmail.com>
2026-09-17 12:12:23 +0900

    新しいの

---
+++ clojure-destructuring/spec/clojure_destructuring/core_spec.clj
@@ -0,0 +1,7 @@
+(ns clojure-destructuring.core-spec
+  (:require [clojure-destructuring.core :refer :all]
+            [speclj.core :refer :all]))
+
+(describe "test"
+          (it "test2"
+              (should= 1 2)))
