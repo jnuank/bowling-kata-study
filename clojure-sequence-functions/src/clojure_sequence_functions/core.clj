48129c1f69e40d747f11c87bcd7f9711e4cfd2ae
jnuank <ikuta1919@gmail.com>
2026-09-17 09:20:53 +0900

    これで大体3分岐同じ

--- clojure-sequence-functions/src/clojure_sequence_functions/core.clj
+++ clojure-sequence-functions/src/clojure_sequence_functions/core.clj
@@ -10,6 +10,18 @@
     (= 10 (two-roll-sum pins)) :spare
     :else :open))
 
+(defn- frame-score [pins kind]
+  (case kind
+    :strike (+ 10 (two-roll-sum (drop 1 pins)))
+    :spare (+ 10 (first (drop 2 pins)))
+    :open (two-roll-sum pins))
+    )
+
+(defn- advance [pins kind]
+  (case kind
+    :strike (drop 1 pins)
+    (drop 2 pins)))
+
 (defn- score* [pins frame]
   (cond
     (empty? pins)    0
@@ -18,9 +30,9 @@
 
     :else
     (cond
-      (= :strike (frame-kind pins)) (+ 10 (two-roll-sum (drop 1 pins)) (score* (drop 1 pins) (inc frame)))
-      (= :spare (frame-kind pins)) (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) (inc frame)))
-      (= :open (frame-kind pins)) (+ (two-roll-sum pins) (score* (drop 2 pins) (inc frame))))))
+      (= :strike (frame-kind pins)) (+ (frame-score pins (frame-kind pins)) (score* (advance pins (frame-kind pins)) (inc frame)))
+      (= :spare (frame-kind pins)) (+ (frame-score pins (frame-kind pins)) (score* (advance pins (frame-kind pins)) (inc frame)))
+      (= :open (frame-kind pins)) (+ (frame-score pins (frame-kind pins)) (score* (advance pins (frame-kind pins)) (inc frame))))))
 
 (defn score [pins]
   (score* pins 1))
