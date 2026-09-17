7babe110e9c46b47c09cce1efa4df95588120021
jnuank <ikuta1919@gmail.com>
2026-09-18 08:22:10 +0900

    ひとまず通す

--- clojure-data-transform/spec/clojure_data_transform/core_spec.clj
+++ clojure-data-transform/spec/clojure_data_transform/core_spec.clj
@@ -1,6 +1,7 @@
 (ns clojure-data-transform.core-spec
-  (:require [speclj.core :refer [describe it should=]]))
+  (:require [clojure-data-transform.core :refer [score]]
+            [speclj.core :refer [describe it should=]]))
 
-(describe "core"
-  (it "placeholder"
-    (should= true true)))
+(describe "score"
+          (it "全部ガーター"
+              (should= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
