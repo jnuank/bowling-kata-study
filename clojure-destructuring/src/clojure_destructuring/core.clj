e61747c6387e08bdad57cc3a37e91d066babc570
jnuank <ikuta1919@gmail.com>
2026-09-17 21:43:52 +0900

    concatもfistも消してみた

--- clojure-destructuring/src/clojure_destructuring/core.clj
+++ clojure-destructuring/src/clojure_destructuring/core.clj
@@ -2,22 +2,22 @@
   (:gen-class))
 
 (defn- score* [pins frame]
-  (let [[a b & [next-pin :as remaining-pins] :as pins] pins]
+  (let [[a & [b & [next-pin :as after-two] :as remaining-pins] :as pins] pins]
     (cond
       (empty? pins)
       0
-      
+
       (= frame 10)
       (reduce + pins)
 
       (= 10 a)
-      (+ 10 b next-pin (score* (concat [b] remaining-pins) (inc frame)))
+      (+ 10 b next-pin (score* remaining-pins (inc frame)))
 
       (= 10 (+ a b))
-      (+ 10 (first remaining-pins) (score* remaining-pins (inc frame)))
+      (+ 10 next-pin (score* after-two (inc frame)))
 
       :else
-      (+ a b (score* remaining-pins (inc frame))))))
+      (+ a b (score* after-two (inc frame))))))
 
 (defn score [pins]
   (score* pins 1))
