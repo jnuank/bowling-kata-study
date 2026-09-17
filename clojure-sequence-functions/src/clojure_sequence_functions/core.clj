87fc36d84e492a3dc2ed70df6397f4c359bd3bb0
jnuank <ikuta1919@gmail.com>
2026-09-17 09:04:59 +0900

    two-roll-sum

--- clojure-sequence-functions/src/clojure_sequence_functions/core.clj
+++ clojure-sequence-functions/src/clojure_sequence_functions/core.clj
@@ -1,6 +1,8 @@
 (ns clojure-sequence-functions.core
   (:gen-class))
 
+(defn- two-roll-sum [pins]
+  (reduce + (take 2 pins)))
 
 (defn- score* [pins frame]
   (cond
@@ -11,13 +13,13 @@
     (reduce + pins)
 
     :else
-    (let [spare? (= 10 (reduce + (take 2 pins)))
+    (let [spare? (= 10 (two-roll-sum pins))
           strike? (= 10 (first pins))
           next-frame (inc frame)]
       (cond
-        strike? (+ 10 (reduce + (take 2 (drop 1 pins))) (score* (drop 1 pins) next-frame))
+        strike? (+ 10 (two-roll-sum (drop 1 pins)) (score* (drop 1 pins) next-frame))
         spare? (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) next-frame))
-        :else (+ (reduce + (take 2 pins)) (score* (drop 2 pins) next-frame))))))
+        :else (+ (two-roll-sum pins) (score* (drop 2 pins) next-frame))))))
 
 (defn score [pins]
   (score* pins 1))
