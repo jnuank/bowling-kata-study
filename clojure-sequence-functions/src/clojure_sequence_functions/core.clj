0416f9ff6d64e7d19d9081a108a1df8d95654c77
jnuank <ikuta1919@gmail.com>
2026-09-17 09:09:42 +0900

    frame-kind

--- clojure-sequence-functions/src/clojure_sequence_functions/core.clj
+++ clojure-sequence-functions/src/clojure_sequence_functions/core.clj
@@ -4,6 +4,12 @@
 (defn- two-roll-sum [pins]
   (reduce + (take 2 pins)))
 
+(defn- frame-kind [pins]
+  (cond
+    (= 10 (first pins)) :strike
+    (= 10 (two-roll-sum pins)) :spare
+    :else :open))
+
 (defn- score* [pins frame]
   (cond
     (empty? pins)
@@ -13,13 +19,11 @@
     (reduce + pins)
 
     :else
-    (let [spare? (= 10 (two-roll-sum pins))
-          strike? (= 10 (first pins))
-          next-frame (inc frame)]
+    (let [next-frame (inc frame)]
       (cond
-        strike? (+ 10 (two-roll-sum (drop 1 pins)) (score* (drop 1 pins) next-frame))
-        spare? (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) next-frame))
-        :else (+ (two-roll-sum pins) (score* (drop 2 pins) next-frame))))))
+        (= :strike (frame-kind pins)) (+ 10 (two-roll-sum (drop 1 pins)) (score* (drop 1 pins) next-frame))
+        (= :spare (frame-kind pins)) (+ 10 (first (drop 2 pins)) (score* (drop 2 pins) next-frame))
+        (= :open (frame-kind pins)) (+ (two-roll-sum pins) (score* (drop 2 pins) next-frame))))))
 
 (defn score [pins]
   (score* pins 1))
