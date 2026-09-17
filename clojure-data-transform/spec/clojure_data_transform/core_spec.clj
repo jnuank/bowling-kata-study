fb688dac196e6e96df87b9aca404d845196ef23f
jnuank <ikuta1919@gmail.com>
2026-09-18 08:26:12 +0900

    frameという概念

--- clojure-data-transform/spec/clojure_data_transform/core_spec.clj
+++ clojure-data-transform/spec/clojure_data_transform/core_spec.clj
@@ -1,7 +1,12 @@
 (ns clojure-data-transform.core-spec
-  (:require [clojure-data-transform.core :refer [score]]
+  (:require [clojure-data-transform.core :refer [score frames]]
             [speclj.core :refer [describe it should=]]))
 
 (describe "score"
           (it "全部ガーター"
               (should= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
+
+(describe "frames"
+          (it "全部ガーター"
+              (should= [[0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0]]
+                       (frames [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
--- clojure-data-transform/src/clojure_data_transform/core.clj
+++ clojure-data-transform/src/clojure_data_transform/core.clj
@@ -1,5 +1,8 @@
 (ns clojure-data-transform.core
   (:gen-class))
 
+(defn frames [rolls]
+  [[0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0] [0 0]])
+
 (defn score [rolls]
   (reduce + rolls))
