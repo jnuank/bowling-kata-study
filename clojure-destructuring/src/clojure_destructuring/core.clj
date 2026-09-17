66176f246ab2c4e59b21e16fb6c73d47871e18bf
jnuank <ikuta1919@gmail.com>
2026-09-17 21:04:54 +0900

    ようやく通った

--- clojure-destructuring/src/clojure_destructuring/core.clj
+++ clojure-destructuring/src/clojure_destructuring/core.clj
@@ -2,7 +2,13 @@
   (:gen-class))
 
 (defn score [pins]
-  (let [[a b & pins] pins]
-    (if (= 10 (+ a b))
-      (+ 10 a (+ b (first pins)) (score pins))
-      (+ a b (score pins))))) 
+  (let [[a b & remaining-pins :as pins] pins]
+    (cond 
+      (empty? pins)
+      0
+      
+      (= 10 (+ a b))
+      (+ 10 (first remaining-pins) (score remaining-pins))
+      
+      :else
+      (+ a b (score remaining-pins)))))
