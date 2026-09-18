d49f9683fdd3f1c16ff13fc520e8debeb6f35cd0
jnuank <ikuta1919@gmail.com>
2026-09-18 19:58:14 +0900

    scoresを作る

--- clojure-data-transform/src/clojure_data_transform/core.clj
+++ clojure-data-transform/src/clojure_data_transform/core.clj
@@ -2,15 +2,15 @@
   (:gen-class))
 
 (defn frames [rolls]
-  (partition 2 rolls)
-  )
+  (partition 2 rolls))
+
+(defn scores [frames]
+  (map (partial apply +) frames))
 
 (defn score [rolls]
   (->> rolls
        frames
-       (map (partial apply +))
-       (reduce +)
-       )
-  )
+       scores
+       (reduce +)))
 
 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0])
