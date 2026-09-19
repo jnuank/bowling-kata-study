3c79be2a08cc89988e3249f5af4dde917f21efec
jnuank <ikuta1919@gmail.com>
2026-09-19 20:36:54 +0900

    いい感じでは

--- clojure-data-transform/src/clojure_data_transform/core.clj
+++ clojure-data-transform/src/clojure_data_transform/core.clj
@@ -1,11 +1,22 @@
 (ns clojure-data-transform.core
   (:gen-class))
 
+(defn scoring-rolls-per-frame* [[first-roll second-roll bonus-roll :as rolls] frame-count]
+  (cond
+    (or (empty? rolls) (> frame-count 10)) []
+    (= 10 first-roll) (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (rest rolls) (inc frame-count)))
+    :else (cons [first-roll second-roll] (scoring-rolls-per-frame* (drop 2 rolls) (inc frame-count)))))
+
 (defn scoring-rolls-per-frame [rolls]
-  (partition 2 rolls)
-  )
+  (scoring-rolls-per-frame* rolls 1))
 
 (defn game-score [rolls]
-  0)
+  (->> rolls
+       scoring-rolls-per-frame
+       (map #(reduce + %))
+       (reduce +)))
 
 
+(take 2 [1 2 3 4 5])
+
+(scoring-rolls-per-frame [10 10 10 10 10 10 10 10 10 10 10 10])
