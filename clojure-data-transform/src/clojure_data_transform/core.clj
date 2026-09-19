0976703718c5fc214f1d0d78c00e234623716c3d
jnuank <ikuta1919@gmail.com>
2026-09-19 21:16:46 +0900

    tidy

--- clojure-data-transform/src/clojure_data_transform/core.clj
+++ clojure-data-transform/src/clojure_data_transform/core.clj
@@ -2,11 +2,15 @@
   (:gen-class))
 
 (defn scoring-rolls-per-frame* [[first-roll second-roll bonus-roll :as rolls] frame-count]
-  (cond
-    (or (empty? rolls) (> frame-count 10)) []
-    (= 10 first-roll) (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (rest rolls) (inc frame-count)))
-    (= 10 (+ first-roll second-roll)) (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (drop 2 rolls) (inc frame-count)))
-    :else (cons [first-roll second-roll] (scoring-rolls-per-frame* (drop 2 rolls) (inc frame-count)))))
+  (let [next-frame (inc frame-count)
+        strike? (= 10 first-roll)
+        spare? (and second-roll (= 10 (+ first-roll second-roll)))
+        game-set? (or (empty? rolls) (> frame-count 10))]
+   (cond
+    game-set? []
+    strike? (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (rest rolls) next-frame))
+    spare? (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (drop 2 rolls) next-frame))
+    :else (cons [first-roll second-roll] (scoring-rolls-per-frame* (drop 2 rolls) next-frame)))))
 
 (defn scoring-rolls-per-frame [rolls]
   (scoring-rolls-per-frame* rolls 1))
