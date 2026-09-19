afafb91b2561961714792e8712bfbb310d46d43e
jnuank <ikuta1919@gmail.com>
2026-09-19 21:32:33 +0900

    game-set概念

--- clojure-data-transform/src/clojure_data_transform/core.clj
+++ clojure-data-transform/src/clojure_data_transform/core.clj
@@ -1,16 +1,23 @@
 (ns clojure-data-transform.core
   (:gen-class))
 
+(defn- frame? [rolls]
+  (let [first-roll (first rolls)
+        second-roll (second rolls) ]
+    (cond
+      (= 10 first-roll) :strike
+      (= 10 (and second-roll (+ first-roll second-roll))) :spare
+      :else :open)))
+
 (defn scoring-rolls-per-frame* [[first-roll second-roll bonus-roll :as rolls] frame-count]
   (let [next-frame (inc frame-count)
-        strike? (= 10 first-roll)
-        spare? (and second-roll (= 10 (+ first-roll second-roll)))
         game-set? (or (empty? rolls) (> frame-count 10))]
-   (cond
-    game-set? []
-    strike? (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (rest rolls) next-frame))
-    spare? (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (drop 2 rolls) next-frame))
-    :else (cons [first-roll second-roll] (scoring-rolls-per-frame* (drop 2 rolls) next-frame)))))
+    (if game-set?
+      []
+    (case (frame? rolls)
+      :strike (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (rest rolls) next-frame))
+      :spare (cons [first-roll second-roll bonus-roll] (scoring-rolls-per-frame* (drop 2 rolls) next-frame))
+      :open (cons [first-roll second-roll] (scoring-rolls-per-frame* (drop 2 rolls) next-frame))))))
 
 (defn scoring-rolls-per-frame [rolls]
   (scoring-rolls-per-frame* rolls 1))
