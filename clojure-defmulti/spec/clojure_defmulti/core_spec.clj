41b2584e8d82e2c91e9d6ed307adc11a86d78562
jnuank <ikuta1919@gmail.com>
2026-09-20 11:01:24 +0900

    落ちる

--- clojure-defmulti/spec/clojure_defmulti/core_spec.clj
+++ clojure-defmulti/spec/clojure_defmulti/core_spec.clj
@@ -3,6 +3,11 @@
             [speclj.core :refer [describe it should=]]))
 
 (describe "score"
-
           (it "全ガーター"
-              (should= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
+              (should= 0 (score [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0])))
+          
+          (it "1フレーム目が4と5。その後はガーター"
+              (should= 9 (score [4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0])))
+          
+          (it "1フレーム目がスペア。2フレーム目が4と5。その後はガーター"
+              (should= 23 (score [4 6 4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
