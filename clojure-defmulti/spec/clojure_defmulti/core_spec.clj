cec79f0df489a9e270db20ff364a3e95eaf29531
jnuank <ikuta1919@gmail.com>
2026-09-20 11:58:35 +0900

    defmultiでやってみた

--- clojure-defmulti/spec/clojure_defmulti/core_spec.clj
+++ clojure-defmulti/spec/clojure_defmulti/core_spec.clj
@@ -1,5 +1,5 @@
 (ns clojure-defmulti.core-spec
-  (:require [clojure-defmulti.core :refer [score]]
+  (:require [clojure-defmulti.core :refer [score frame-type]]
             [speclj.core :refer [describe it should=]]))
 
 (describe "score"
@@ -10,4 +10,13 @@
               (should= 9 (score [4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0])))
           
           (it "1フレーム目がスペア。2フレーム目が4と5。その後はガーター"
-              (should= 23 (score [4 6 4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]))))
+              (should= 23 (score [4 6 4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0])))
+
+          (it "1フレーム目がストライク。2フレーム目が4と5。その後はガーター"
+              (should= 28 (score [10 4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0])))
+
+          (it "全フレームスペア"
+              (should= 150 (score [5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5])))
+          
+          (it "全フレームストライク"
+              (should= 300 (score [10 10 10 10 10 10 10 10 10 10 10 10]))))
--- clojure-defmulti/src/clojure_defmulti/core.clj
+++ clojure-defmulti/src/clojure_defmulti/core.clj
@@ -1,5 +1,38 @@
 (ns clojure-defmulti.core
   (:gen-class))
 
+(defmulti frame-type
+  "フレームのロールからタイプを判定する"
+  (fn [rolls]
+    (let [first (first rolls)
+          second (second rolls)]
+      (cond
+        (= 3 (count rolls)) :last
+        (= first 10) :strike
+        (= (+ (or first 0) (or second 0)) 10) :spare
+        :else :open))))
+
+(defmethod frame-type :strike [rolls]
+  {:score (reduce + (take 3 rolls)) :rest (rest rolls)})
+
+(defmethod frame-type :spare [rolls]
+  {:score (+ 10 (nth rolls 2)) :rest (drop 2 rolls)})
+
+(defmethod frame-type :open [rolls]
+  {:score (reduce + (take 2 rolls)) :rest (drop 2 rolls)})
+
+(defmethod frame-type :last [rolls]
+  {:score (reduce + rolls) :rest []})
+
+(defn frame-score [rolls]
+  (if (empty? rolls)
+    []
+    (let [frame (frame-type rolls)]
+      (cons (:score frame) (frame-score (:rest frame))))))
+
 (defn score [rolls]
-  (reduce + rolls))
+  (->> rolls
+       frame-score
+       (reduce +)))
+
+
