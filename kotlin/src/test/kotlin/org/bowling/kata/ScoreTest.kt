9f69a2c2b621c863b41adba7b56a89f82026452e
jnuank <ikuta1919@gmail.com>
2026-09-21 12:41:36 +0900

    tidy

--- kotlin/src/test/kotlin/org/bowling/kata/ScoreTest.kt
+++ kotlin/src/test/kotlin/org/bowling/kata/ScoreTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 
-class GameTest: StringSpec({
+class ScoreTest: StringSpec({
     "すべてガーター" {
         val score = score(listOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
 
