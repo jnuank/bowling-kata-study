00803f7e500705714ef62dd2b905399692f07420
jnuank <ikuta1919@gmail.com>
2026-09-21 12:40:02 +0900

    次のパターン

--- kotlin/src/test/kotlin/org/bowling/kata/MainTest.kt
+++ kotlin/src/test/kotlin/org/bowling/kata/MainTest.kt
@@ -10,4 +10,11 @@ class GameTest: StringSpec({
 
         score shouldBe 0
     }
+
+    "1フレームが4と5。後はガーター" {
+        val score = score(listOf(4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+
+        score shouldBe 9
+    }
+
 })
