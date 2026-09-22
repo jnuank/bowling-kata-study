20d4e69a796151b3d12ac1bb4d722c56ff7fc502
jnuank <ikuta1919@gmail.com>
2026-09-23 08:58:46 +0900

    なかなかやばい

--- kotlin/src/main/kotlin/org/bowling/kata/Game.kt
+++ kotlin/src/main/kotlin/org/bowling/kata/Game.kt
@@ -13,6 +13,12 @@ fun score(rolls: List<Int>): Int {
     var total = 0
     var index = 0
     while (index < rolls.size) {
+        if(rolls.size - index == 3){
+            total += rolls[index] + rolls[index + 1] + rolls[index + 2]
+            index += 3
+            continue
+        }
+
         if(10 == rolls[index] + rolls[index+1]){
             total += 10 + rolls[index+2]
             index +=2
--- kotlin/src/test/kotlin/org/bowling/kata/ScoreTest.kt
+++ kotlin/src/test/kotlin/org/bowling/kata/ScoreTest.kt
@@ -6,21 +6,22 @@ import io.kotest.matchers.shouldBe
 
 class ScoreTest : StringSpec({
     "すべてガーター" {
-
         val score = score(List(20) { 0 })
-
         score shouldBe 0
     }
 
     "1フレームが4と5。後はガーター" {
         val score = score(listOf(4, 5) + List(18) { 0 })
-
         score shouldBe 9
     }
 
     "1フレーム目がスペア、2フレーム目が4と5。後はガーター" {
         val score = score(listOf(4, 6, 4, 5) + List(16) { 0 })
-
         score shouldBe 23
+    }
+
+    "すべてスペア" {
+        val score = score(List(21) { 5 })
+        score shouldBe 150
     }
 })
