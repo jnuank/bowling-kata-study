3eec556d06a21563998acdd75f906676a02a7fb7
jnuank <ikuta1919@gmail.com>
2026-09-24 09:05:50 +0900

    別のkotlin

---
+++ kotlin-polymorphism/src/test/kotlin/MainTest.kt
@@ -0,0 +1,10 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class MainTest : StringSpec({
+    "test" {
+        true shouldBe true
+    }
+
+})
