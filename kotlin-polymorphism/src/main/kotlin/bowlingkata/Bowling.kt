e40c28082f7620729ea43ae1addd9b4fabe04072
jnuank <ikuta1919@gmail.com>
2026-09-24 22:23:48 +0900

    かなり違和感

--- kotlin-polymorphism/src/main/kotlin/bowlingkata/Bowling.kt
+++ kotlin-polymorphism/src/main/kotlin/bowlingkata/Bowling.kt
@@ -1,5 +1,53 @@
 package org.example.bowlingkata
 
+sealed interface Frame2 {
+    fun score(): Int
+    fun firstPins(): Int
+    fun firstTwoPins(): Int
+}
+
+class Strike(private val next: Frame2) : Frame2 {
+    override fun score(): Int = 10 + next.firstTwoPins() + next.score()
+
+    override fun firstPins(): Int = 10
+
+    override fun firstTwoPins(): Int = 10 + next.firstPins()
+
+}
+
+class Spare(private val first: Int, val next: Frame2) : Frame2 {
+    override fun score(): Int = 10 + next.firstPins() + next.score()
+
+    override fun firstPins(): Int = first
+
+    override fun firstTwoPins(): Int = 10
+
+}
+
+class Open(private val first: Int, private val second: Int, val next: Frame2) : Frame2 {
+    override fun score(): Int = first + second + next.score()
+
+    override fun firstPins(): Int = first
+
+    override fun firstTwoPins(): Int = first + second
+
+}
+
+class Bonus(private val rolls: List<Int>) : Frame2 {
+    override fun score(): Int = 0
+
+    override fun firstPins(): Int = rolls.take(1).sum()
+
+    override fun firstTwoPins(): Int = rolls.take(2).sum()
+}
+
+private fun parse(rolls: List<Int>, frameNo: Int = 1): Frame2 = when {
+    frameNo > 10 -> Bonus(rolls)
+    rolls[0] == 10 -> Strike(parse(rolls.drop(1), frameNo + 1))
+    rolls[0] + rolls[1] == 10 -> Spare(rolls[0], parse(rolls.drop(2), frameNo + 1))
+    else -> Open(rolls[0], rolls[1], parse(rolls.drop(2), frameNo + 1))
+}
+
 sealed class Frame(val rollCount: Int, private val bonusCount: Int) {
     fun score(rolls: List<Int>): Int = rolls.take(rollCount + bonusCount).sum()
 
@@ -17,13 +65,5 @@ sealed class Frame(val rollCount: Int, private val bonusCount: Int) {
 }
 
 fun score(rolls: List<Int>): Int  {
-    var total = 0
-    var rest = rolls
-
-    repeat(10) {
-        val frame = Frame.from(rest)
-        total += frame.score(rest)
-        rest = rest.drop(frame.rollCount)
-    }
-    return total
+    return parse(rolls).score()
 }
