7642ebe070f3be34179dae6ec75eedf0878cfb78
jnuank <ikuta1919@gmail.com>
2026-09-24 09:20:21 +0900

    むりやり

--- kotlin-polymorphism/src/main/kotlin/bowlingkata/Bowling.kt
+++ kotlin-polymorphism/src/main/kotlin/bowlingkata/Bowling.kt
@@ -1,3 +1,16 @@
 package org.example.bowlingkata
 
-fun score(rolls: List<Int>) = 0
+fun score(rolls: List<Int>): Int  {
+    var total = 0
+    var index = 0
+    while (index < rolls.size) {
+        if(rolls[index] + rolls[index+1] == 10) {
+            total += rolls[index] + rolls[index+1] + rolls[index+2]
+            index += 2
+        } else {
+            total += rolls[index] + rolls[index+1]
+            index += 2
+        }
+    }
+    return total
+}
